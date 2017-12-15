94b711c77c71c6a60e2cd731ec7459ecf613eb2d
ruchira <rbn150030@utdallas.edu>
2017-12-15 03:33:21 -0600

    added visualization and noun extraction

--- googleNewsAPI.py
+++ googleNewsAPI.py
@@ -1,8 +1,9 @@
 import json
-import string
+import sys
+import unicodedata
 from collections import defaultdict
 
-# import gmplot
+import gmplot
 import nltk
 import requests
 from newspaper import Article
@@ -12,7 +13,7 @@ from pyspark.ml.linalg import Vectors
 from pyspark.sql import SparkSession
 
 nltk.download('stopwords')
-from nltk.corpus import stopwords
+nltk.download('averaged_perceptron_tagger')
 
 # setting up Spark Context
 conf = SparkConf().setMaster("local").setAppName("project")
@@ -23,7 +24,7 @@ spark = SparkSession \
 
 # using Google News API for collecting articles
 url = ('https://newsapi.org/v2/everything?'
-       'q=Apple&'
+       'q=murder&'
        'from=2017-11-19&'
        'sortBy=popularity&'
        'apiKey=a562d2fdb25846999ae74f07e83d467d'
@@ -52,7 +53,9 @@ for i in jsontoPy['articles']:
 
 dictXY = defaultdict(list)
 dictCoor = defaultdict(list)
-
+tbl = dict.fromkeys(i for i in range(sys.maxunicode)
+                    if unicodedata.category(chr(i)).startswith('P'))
+# print(tbl)
 text = {}
 id = 0
 for i in dictUrl.keys():
@@ -62,9 +65,15 @@ for i in dictUrl.keys():
     article.parse()
     articleText = article.text
     if len(articleText) < 10000:
-        a = " ".join([word for word in articleText.lower().translate(str.maketrans('', '', string.punctuation)).split()
-                      if word not in stopwords.words('english')])
-        text[id] = a  # save article text
+        # b = articleText.translate(tbl)
+        # a = " ".join([word for word in b.lower().translate(str.maketrans('', '', string.punctuation)).split()
+        #               if word not in stopwords.words('english')])
+        is_noun = lambda pos: pos[:2] == 'NN'
+        tokenized = nltk.word_tokenize(articleText)
+        nouns = " ".join([word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)])
+        # for noun in nouns:
+        #     print('noun: '+noun)
+        text[id] = nouns.translate(tbl)  # save article text
         # print(articleText)
 
         # using Geoparser.io for location extraction from the articles
@@ -85,7 +94,6 @@ for i in dictUrl.keys():
             coorString = str(geoCoord[0]) + ',' + str(geoCoord[1])
             dictCoor[coorString].append(id)
         id += 1
-
 
 # print(dictXY)
 
@@ -128,7 +136,9 @@ print(clustersWithArticles.take(10))
 def get_article_text(article_ids):
     article_text = ''
     for article_id in article_ids:
-        article_text = article_text + text.get(article_id)
+        txt = text.get(article_id)
+        if (len(txt) > 1):
+            article_text = article_text + txt
     return article_text
 
 
@@ -140,6 +150,7 @@ def get_top_ten_words(lines):
     line_words = lines.split(" ")
     words_rdd = sc.parallelize(line_words)
     counts = words_rdd.flatMap(lambda line: line.split(" ")) \
+        .filter(lambda x: len(x) > 1) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a + b) \
         .sortBy(lambda a: a[1], ascending=False)
@@ -152,19 +163,40 @@ top10Words = []
 for r in clusterText:
     top10Words.append(get_top_ten_words(r))
 # print(top10Words)
-lat = []
-lng = []
+# lat = []
+# lng = []
 
 centers = model.clusterCenters()
 print("Cluster Centers: ")
 for center in centers:
-    lat.append(center[1])
-    lng.append(center[0])
+    # lat.append(center[1])
+    # lng.append(center[0])
     print(center)
 
-clusterRDD = clustersWithText.map(lambda x: (centers[x[0]][1], centers[x[0]][0], top10Words[x[0]]))
+clusterRDD = clustersWithText.map(
+    lambda x: (str(centers[x[0]][1]) + ':*:' + str(centers[x[0]][0]) + ':*:' + str(top10Words[x[0]])))
 print(clusterRDD.take(1))
 clusterRDD.saveAsTextFile("clusters.txt")
+
+lat = []
+lng = []
+tittles = []
+gmap = gmplot.GoogleMapPlotter(0, 0, 2, apikey=' AIzaSyDLddgAEB0qY8PLEHr-DF-YXPqoK3HdF7E ')
+
+with open('clusters.txt/part-00000') as inFile:
+    for line in inFile.readlines():
+        words = line.split(":*:")
+        if len(words) == 3:
+            lat.append(float(words[0]))
+            lng.append(float(words[1]))
+            keywords = words[2].rstrip()
+            tittles.append(keywords)
+            gmap.marker(float(words[0]), float(words[1]), '#3B0B39', title=keywords)
+
+gmap = gmplot.GoogleMapPlotter(0, 0, 2, apikey=' AIzaSyDLddgAEB0qY8PLEHr-DF-YXPqoK3HdF7E ')
+gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s/"
+gmap.scatter(lat, lng, tittles, 'cornflowerblue', edge_width=10, marker=True)
+gmap.draw('top10.html')
 
 
 # using Google Map API for mapping the coordinates
